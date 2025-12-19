6bb3700f835f88039f4585f5e73706abbede6218
bala-amypo <bala@amypo.in>
2025-12-19 03:56:14 +0000

    User commit

--- src/main/java/com/example/demo/controller/studentController.java
+++ src/main/java/com/example/demo/controller/studentController.java
@@ -1,44 +1,46 @@
- 
-package com.example.demo.controller; 
- 
-import java.util.List; 
-import jakarta.validation.Valid; 
- 
-import org.springframework.beans.factory.annotation.Autowired; 
-import org.springframework.web.bind.annotation.*; 
- 
-import com.example.demo.entity.*; 
-import com.example.demo.service.*; 
- 
-@RestController 
-public class studentController { 
- 
-    @Autowired 
-    studentService service; 
- 
-    @GetMapping("/getAllStudent") 
-    public List<studentEntity> getAll() { 
-        return service.getAll(); 
-    } 
- 
-    @PostMapping("/addStudent") 
-    public studentEntity addStudent(@Valid @RequestBody studentEntity stu) { 
-        return service.addStudent(stu); 
-    } 
- 
-    @GetMapping("/student/{id}") 
-    public studentEntity getById(@PathVariable Long id) { 
-        return service.getById(id); 
-    } 
- 
-    @PutMapping("/update/{id}") 
-    public String updateStudent(@PathVariable Long id, @Valid @RequestBody studentEntity 
-stu) { 
-        return service.updateStudent(id, stu); 
-    } 
- 
-    @DeleteMapping("/delete/{id}") 
-    public String deleteStudent(@PathVariable Long id) { 
-        return service.deleteStudent(id); 
-    } 
-} 
+studentController.java
+package com.example.demo.controller;
+
+import com.example.demo.entity.studentEntity;
+import com.example.demo.service.studentService;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+public class studentController {
+
+    @Autowired
+    studentService service;
+
+    @GetMapping("/getAllStudent")
+    public List<studentEntity> getAll() {
+        return service.getAll();
+    }
+
+    @PostMapping("/add")
+    public studentEntity addStudent(
+            @Valid @RequestBody studentEntity student) {
+        return service.addStudent(student);
+    }
+
+    @GetMapping("/get/{id}")
+    public studentEntity getStudentById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public String updateStudent(
+            @PathVariable Long id,
+            @RequestBody studentEntity student) {
+        return service.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable long id) {
+        return service.deleteStudent(id);
+    }
+}
--- src/main/java/com/example/demo/exception/globalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/globalExceptionHandler.java
@@ -1,42 +1,34 @@
-package com.example.demo.exception; 
- 
-import java.time.LocalDateTime; 
-import org.springframework.http.*; 
-import org.springframework.web.bind.annotation.*; 
- 
-@RestControllerAdvice 
-public class GlobalExceptionHandler { 
- 
-    @ExceptionHandler(ResourceNotFoundException.class) 
-    public ResponseEntity<Object> handleResourceNotFound(ResourceNotFoundException 
-ex) { 
- 
-        ErrorResponse error = new ErrorResponse( 
-            LocalDateTime.now(), 
-            ex.getMessage(), 
-            HttpStatus.NOT_FOUND.value() 
-        ); 
- 
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND); 
-    } 
- 
-@ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.clas
-s) 
-    public ResponseEntity<Object> 
-handleValidationErrors(org.springframework.web.bind.MethodArgumentNotValidException 
-ex) { 
- 
-        String errorMsg = ex.getBindingResult() 
-            .getFieldErrors() 
-            .get(0) 
-            .getDefaultMessage(); 
- 
-            ErrorResponse error = new ErrorResponse( 
-                java.time.LocalDateTime.now(), 
-                errorMsg, 
-                HttpStatus.BAD_REQUEST.value() 
-            ); 
- 
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST); 
-    } 
-} 
+globalExceptionHandler.java
+package com.example.demo.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+@RestControllerAdvice
+public class globalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleFieldError(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> error = new HashMap<>();
+
+        ex.getBindingResult()
+          .getFieldErrors()
+          .forEach(err ->
+              error.put(err.getField(), err.getDefaultMessage())
+          );
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<?> handleStudentNotValid(StudentNotFoundException ex){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+    }
+}
